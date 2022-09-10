9c06e80d0cb3466815bd254b96ff0ee187641fd3
kavi123 <kavyaharshiraj@gmail.com>
2022-09-10 09:48:54 +0530

    new code added by kavya

--- src/org/computer/Desktop.java
+++ src/org/computer/Desktop.java
@@ -5,7 +5,7 @@ public class Desktop implements HardWare,Software {
 	@Override
 	public void softwareResources() {
 	System.out.println("Intel");
-	System.out.println("5");
+	System.out.println("52");
 	}
 
 	@Override
